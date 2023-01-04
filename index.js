95d30f6130fcbd1cbfcb4abfa9b92c96329e24d5
fmota89 <fmota@student.42lisboa.com>
2023-01-04 13:31:46 +0000

    Add event listener loop

---
+++ index.js
@@ -0,0 +1,10 @@
+
+let numberOfDrumButtons = document.querySelectorAll(".drum").length;
+
+for (let i = 0; i < numberOfDrumButtons; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+    alert("I got clicked!");
+
+
+  });
+}
