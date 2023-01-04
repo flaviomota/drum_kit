4601f63d028f4d4bd5e97fc7750bacdad66f95d8
fmota89 <fmota@student.42lisboa.com>
2023-01-04 15:34:42 +0000

    all done and reviewed

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Detecting Button Press
 
 let numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
@@ -6,44 +7,75 @@ for (let i = 0; i < numberOfDrumButtons; i++) {
 
     let buttonInnerHTML = this.innerHTML;
 
-    switch (buttonInnerHTML) {
-      case "w":
-        let tom1 = new Audio("sounds/tom-1.mp3");
-        tom1.play();
-        break;
+    makeSound(buttonInnerHTML);
 
-      case "a":
-        let tom2 = new Audio("sounds/tom-2.mp3");
-        tom2.play();
-        break;
+    buttonAnimation(buttonInnerHTML);
 
-      case "s":
-        let tom3 = new Audio("sounds/tom-3.mp3");
-        tom3.play();
-        break;
-
-      case "d":
-        let tom4 = new Audio("sounds/tom-4.mp3")
-        tom4.play();
-        break;
-
-      case "j":
-        let snare = new Audio("sounds/snare.mp3");
-        snare.play();
-        break;
-
-      case "k":
-        let crash = new Audio("sounds/crash.mp3");
-        crash.play();
-        break;
-
-      case "l":
-        let kickBass = new Audio("sounds/kick-bass.mp3");
-        kickBass.play();
-        break;
-
-      default:
-        console.log(buttonInnerHTML);
-    }
   });
+}
+
+// Detecting Keyboard Press
+
+document.addEventListener("keydown", function (event) {
+
+  makeSound(event.key);
+
+  buttonAnimation(event.key);
+
+});
+
+function makeSound(key) {
+
+  switch (key) {
+    case "w":
+      let tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+
+    case "a":
+      let tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+
+    case "s":
+      let tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      break;
+
+    case "d":
+      let tom4 = new Audio("sounds/tom-4.mp3")
+      tom4.play();
+      break;
+
+    case "j":
+      let snare = new Audio("sounds/snare.mp3");
+      snare.play();
+      break;
+
+    case "k":
+      let crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      break;
+
+    case "l":
+      let kickBass = new Audio("sounds/kick-bass.mp3");
+      kickBass.play();
+      break;
+
+    default:
+      console.log(buttonInnerHTML);
+  }
+
+}
+
+function buttonAnimation(currentKey) {
+
+  let activeButton = document.querySelector("." + currentKey);
+
+  activeButton.classList.add("pressed");
+
+  setTimeout(function () {
+    activeButton.classList.remove("pressed");
+  }, 100);
+
 }
